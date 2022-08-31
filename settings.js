b6d8169c4a4287dacd958e2ffc5ffc9b67cafb7b
QUEEN-ALEXY-MD <112531475+QUEEN-ALEXY-MD@users.noreply.github.com>
2022-08-31 10:17:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,24 +1,4 @@
 //════════════════════════════//
-//If you want to recode, reupload
-//or copy the codes/script,
-//pls give credit
-//no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Cheems Bot MD
-//Thank you to Lord Buddha, Family and Myself
-//════════════════════════════//
-//recode kar ke youtube pe upload kar rhe hai ya
-//codes copy kar ke apne script me dal rhe
-//hai to, description me xeon ka yt channel
-// ka link paste kr dena as a cradit or github 
-//repo me bhi tag kardena baki jo
-//bhi karna hai apki marzi, thank you!🦄
-//════════════════════════════//
-//If you recode and uploading on your channel
-//or copy pasting the codes in ur script, 
-//i give permission to do as long as you
-//put Xeons youtube channel link in the video
-//description and tag me on githuh repo, 
-//thank you🦄
 //════════════════════════════//
 
 const fs = require('fs')
@@ -39,7 +19,7 @@ global.APIKeys = {
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
@@ -54,23 +34,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['94714831387'] //ur owner number
+global.ownername = "🫅Qᴜᴇᴇɴ ᴀʟᴇxʏ-ᴍᴅ ʙᴏᴛ" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
+global.owner = ['94714831387']
+global.ownertag = '94714831387' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.linkz = "https://youtube.com/channel/UCPVxr1KqHXWom1WMUujGLxA" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCPVxr1KqHXWom1WMUujGLxA" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
+global.reactmoji = "📜" //ur menu react emoji
+global.themeemoji = "🫅" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.author = "🫅Qᴜᴇᴇɴ ᴀʟᴇxʏ-ᴍᴅ ʙᴏᴛ" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +59,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94714831387'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -102,23 +82,23 @@ global.ntilink = []
 //omther
 global.sessionName = 'session'
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🧰','📜','💳']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    success: 'ᴅᴏɴᴇ ✅',
+    admin: '*ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ɪꜱ ᴏɴʟʏ ꜰᴏʀ ᴀᴅᴍɪɴ❗*',
+    botAdmin: '*ʙᴏᴛ ᴍᴜꜱᴛ ʙᴇ ᴀᴅᴍᴀɪɴ ꜰɪʀꜱᴛ❗*',
+    owner: '*ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ɪꜱ ᴏɴʟʏ ꜰᴏʀ ᴏᴡɴᴇʀ❗*',
+    group: '*ꜰᴇᴀᴛᴜʀᴇ ᴜꜱᴇᴅ ᴏɴʟʏ ꜰᴏʀ ɢʀᴏᴜᴘꜱ❗*',
+    private: '*ꜰᴇᴀᴛᴜʀᴇꜱ ᴜꜱᴇᴅ ᴏɴʟʏ ꜰᴏʀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ❗*',
+    bot: '*ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ɪꜱ ᴏɴʟʏ ꜰᴏʀ ʙᴏᴛ*',
+    wait: 'ɪɴ ᴘʀᴏᴄᴇꜱꜱ...',
+    linkm: 'ᴡʜᴇʀᴇ ɪꜱ ᴛʜᴇ ʟɪɴᴋ?',
+    error: 'ᴇʀʀᴏʀ❗💻',
+    endLimit: 'ʏᴏᴜʀ ᴅᴀɪʟʏ ʟɪᴍɪᴛ ʜᴀꜱ ᴇxpɪʀᴇᴅ, ᴛʜᴇ ʟɪᴍɪᴛ ᴡɪʟʟ ʙᴇ ʀᴇꜱᴇᴛ ᴇᴠᴇʀʏ 12 ʜᴏᴜʀꜱ',
+    ban: 'ʏᴏᴜ ʜᴀᴠᴇ ʙᴇᴇɴ ʙᴀɴɴᴇᴅ ʙʏ ᴛʜᴇ ᴏᴡɴᴇʀ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʙᴇ ᴜɴʙᴀɴɴᴇᴅ, ᴄʜᴀᴛ ᴏᴡɴᴇʀ.',
+    nsfw: 'ᴛʜᴇ ɴꜱꜰᴡ ꜰᴇᴀᴛᴜʀᴇ ʜᴀꜱ ɴᴏᴛ ʙᴇᴇɴ ᴀᴄᴛɪᴠᴀᴛᴇᴅ, pʟᴘʟᴇᴀꜱᴇ ᴄᴏɴᴛᴀᴄᴛ ᴛʜᴇ ᴀᴅᴍɪɴ ᴛᴏ ᴀᴄᴛɪᴠᴀᴛᴇ',
+    banChat: 'ᴛʜᴇ ʙᴏᴛ ᴡᴀꜱ ʙᴀɴɴᴇᴅ ɪɴ ᴛʜɪꜱ ɢʀᴏᴜᴘ, ᴘʟᴇᴀꜱᴇ ᴄᴏɴᴛᴀɴᴛ ᴛʜᴇ ᴏᴡɴᴇʀ ᴛᴏ ᴜɴʙᴀɴ'
 }
     global.limitawal = {
     premium: "Infinity",
